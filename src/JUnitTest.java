30b6be4cf9002ab7dd20e3adb4600547afee4f3f
Lennart <lennart.nixdorf@gmx.net>
2017-10-18 19:44:43 +0200

    Some changes

--- src/JUnitTest.java
+++ src/JUnitTest.java
@@ -4,25 +4,26 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class JUnitTest {
-	  Project test_simple;
+	
+	Project test_simple;
 
-	  @Before
-	  public void setUp(){
-	    test_simple = new Project(
-	        new String[][]{{"A", "D"}, {"B", "C"}, {"A", "B"}}
+	@Before
+	public void setUp(){
+		test_simple = new Project(
+				new String[][]{{"A", "D"}, {"B", "C"}, {"A", "B"}}
 	    );
-	  }
-
-	  @Test
-	  public void test_simple_example_1() {
-	    assertTrue(test_simple.isWellSorted(new String[]{"A", "D", "B", "C"}));
-	  }
-	  @Test
-	  public void test_simple_example_2() {
-	    assertFalse(test_simple.isWellSorted(new String[]{"D", "A", "B", "C"}));
-	  }
-	  @Test
-	  public void test_simple_example_3() {
-	    assertNotNull(test_simple.isWellSorted(null));
-	  }
+	}
+	
+	@Test
+	public void test_simple_example_1_B() {
+		assertTrue(test_simple.isWellSorted_A(new String[]{"A", "D", "B", "C"}));
+	}
+	@Test
+	public void test_simple_example_2_B() {
+		assertFalse(test_simple.isWellSorted_A(new String[]{"D", "A", "B", "C"}));
+	}
+	@Test
+	public void test_simple_example_3_B() {
+		assertNotNull(test_simple.isWellSorted_A(null));
+	}
 }
