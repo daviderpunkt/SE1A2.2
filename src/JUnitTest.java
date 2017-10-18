3b9e3ed535f59eaa484946d59000b996adf66170
Lennart <lennart.nixdorf@gmx.net>
2017-10-18 19:26:59 +0200

    JunitTest & Project

---
+++ src/JUnitTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JUnitTest {
+	  Project test_simple;
+
+	  @Before
+	  public void setUp(){
+	    test_simple = new Project(
+	        new String[][]{{"A", "D"}, {"B", "C"}, {"A", "B"}}
+	    );
+	  }
+
+	  @Test
+	  public void test_simple_example_1() {
+	    assertTrue(test_simple.isWellSorted(new String[]{"A", "D", "B", "C"}));
+	  }
+	  @Test
+	  public void test_simple_example_2() {
+	    assertFalse(test_simple.isWellSorted(new String[]{"D", "A", "B", "C"}));
+	  }
+	  @Test
+	  public void test_simple_example_3() {
+	    assertNotNull(test_simple.isWellSorted(null));
+;	  }
+}
---
+++ src/Project.java
@@ -0,0 +1,10 @@
+
+public class Project {
+	public String[][] project;
+	public Project(String[][] project) {
+		this.project = project;
+	}
+	public boolean isWellSorted(String[] sequence) {
+		return false;
+	}
+}
