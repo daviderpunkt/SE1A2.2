1ef702ceadb7ac2036f577ee4bc36d993c37ee9a
SHC_LT\Siu <herrcheng@gmail.com>
2017-10-18 19:34:14 +0200

    he

--- src/Project.java
+++ src/Project.java
@@ -1,10 +1,36 @@
 
 public class Project {
 	public String[][] project;
+
 	public Project(String[][] project) {
 		this.project = project;
 	}
-	public boolean isWellSorted(String[] sequence) {
-		return false;
+
+	public boolean isWellSorted_A(String[] sequence) {
+		int len = sequence.length;
+		if (len == 0 && project.length > 0) // Array is empty
+			return false;
+		else if (!check(sequence[len - 1]).isEmpty()) // Last entry is matched
+			return false;
+		else if (len == 1 && !check(sequence[0]).isEmpty()) // One entry and matched
+			return false;
+		else
+			for (int i = 0; i < len - 1; i++) {
+				String find = check(sequence[i]);
+				if (!find.isEmpty())
+					for (int j = i + 1; j < len; j++)
+						if (sequence[j].equals(find))
+							break;
+						else if (j == len - 1 & (!sequence[j].equals(find)))
+							return false;
+			}
+		return true;
+	}
+
+	public String check(String str) {
+		for (int i = 0; i < project.length; i++)
+			if (str.equals(project[i][0]))
+				return project[i][1];
+		return "";
 	}
 }
